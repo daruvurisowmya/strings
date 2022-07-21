b3d9d9a5e81db51051652c4b6ee354fcc21d1757
daruvurisowmya <109509763+daruvurisowmya@users.noreply.github.com>
2022-07-21 01:50:07 -0700

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,44 @@
+# print(type(2.56))
+# print(type(5))
+
+# print('hello world')
+# print('''hello world''')
+# print("hello world")
+# print("""hello world""")
+
+
+# '''working withint,float and str'''
+#  print(54+2.1)
+#  print(type(26+3.8))
+#  print(type(6.23+3.25))
+#  print(type(6.4+2.6))
+#  print('256'+2.6)
+#  print('54'+54)
+
+# print('hello'+54)
+# print('hello'+2.5)
+
+# print(1,2,3,4,5,6)
+#print(1,2,3,4,5,sep='')
+#print(1,2,3,4,5,sep=' ')
+#print(1,2,3,4,5,sep='   ')
+#print(1,2,3,4,5,sep='-')
+
+# print('hello'+'world')
+# print('hello '+'world')
+# print('hello'+' world')
+# print('hello','world')
+# print('hi','sowmya')
+
+# print('hello'+''+'sowmya')
+# print('hello'+' '+'sowmya')
+# print("hello"+" "+"sowmya")
+# print("hello"+"-"+"sowmya")
+
+# print('sowmya'*4)
+# print('sowmya '*4)
+# print('sowmya\n'*4)
+# print('sowmya\n'*4,end='')
+# print('sowmya\n'*7'end=' ')
+# print('sowmya\n',end='')
+# print('sowmya\n',sep='')
